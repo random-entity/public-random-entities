fa781d3fdaedf5477e1ab2be26738908e263c151
random-entity <73927400+random-entity@users.noreply.github.com>
2021-08-12 19:21:08 +0900

    Create items.js

---
+++ out/종교화6/items.js
@@ -0,0 +1,18 @@
+const itemTypes = {
+    RULE: 'RULE',
+    STORY: 'STORY',
+
+    VIDEO: {
+        SEQUENCE: 'SEQUENCE',
+
+    },
+
+    
+    LOCATION: 'LOCATION',
+    APPARATUS: 'APPARATUS',
+}
+
+rule1 = {
+    type: itemTypes.RULE,
+}
+
