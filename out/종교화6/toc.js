2ec2858233110d1a4d1af302827e9068e170ee10
random-entity <73927400+random-entity@users.noreply.github.com>
2021-08-16 19:06:06 +0900

    도큐멘트 업데이트, 목차, top 버튼 추가

---
+++ out/종교화6/toc.js
@@ -0,0 +1,34 @@
+// Get ToC div
+toc = document.getElementById("ToC");
+
+//Add a header
+tocHeader = document.createElement("h2");
+tocHeader.innerText = "목차";
+toc.appendChild(tocHeader);
+
+// Create a list for the ToC entries
+tocList = document.createElement("ul");
+
+// Get the h3 tags - ToC entries
+headers = document.getElementsByTagName("h1");
+
+// For each h3
+for (i = 0; i < headers.length; i++) {
+
+    // Create an id
+    headerId = "h1_" + i;
+    headers[i].id = headerId;
+
+    // a list item for the entry
+    tocListItem = document.createElement("li");
+
+    // a link for the h3
+    tocEntry = document.createElement("a");
+    tocEntry.setAttribute("href", "#" + headerId);
+    tocEntry.innerText = headers[i].innerText;
+
+    tocListItem.appendChild(tocEntry);
+    tocList.appendChild(tocListItem);
+}
+
+toc.appendChild(tocList);
